39171aca9f3c2cf6d37629cc3073a048181786fb
任伟博 <renweibo@xiaomi.com>
2023-04-01 17:21:57 +0000

    do some refactor

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,4 +2,6 @@
 
 nvm install node
 npm install cnpm -g --registry=https://registry.npmmirror.com
-cnpm install -g yo generator-code @vscode/vsce
+cnpm install -g yo generator-code
+# cnpm install --save-dev @vscode/vsce @fabiospampinato/bump
+cnpm install @vscode/vsce @fabiospampinato/bump
