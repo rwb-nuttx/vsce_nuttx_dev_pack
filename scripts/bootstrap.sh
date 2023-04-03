e8d54b2360b9930f75ce297bfae57c6b26287b83
Ren Weibo <renweibo@gmail.com>
2023-04-03 03:18:56 +0000

    [fix] minor update for dev env

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,4 +4,6 @@ nvm install node
 npm install cnpm -g --registry=https://registry.npmmirror.com
 cnpm install -g yo generator-code
 # cnpm install --save-dev @vscode/vsce @fabiospampinato/bump
-cnpm install @vscode/vsce @fabiospampinato/bump
+cnpm install -g @vscode/vsce @fabiospampinato/bump
+git config user.name "Ren Weibo"
+git config user.email "renweibo@gmail.com"
